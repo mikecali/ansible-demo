cbaeda022592569fccbf41686cb433ad48c4f45e
mikecali <mike.calizo@gmail.com>
2018-06-20 11:34:52 -0700

    update motd

--- roles/os-motd/files/motd_update.sh
+++ roles/os-motd/files/motd_update.sh
@@ -5,7 +5,7 @@ LAST_ANSIBLE_RUN=$(date)
 exec >/etc/motd 2>&1
 
 echo ""
-echo "This System is managed by Ansible"
+echo "This System is managed by Ansible for Windy WELLY Meetup"
 echo ""
 echo "  Ansible last run = $LAST_ANSIBLE_RUN"
 echo ""
